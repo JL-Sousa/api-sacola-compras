4770e3c00de5e87b9e2ee4ed2ef21fc87378684b
Jonyelson Lopes <jonyelson20@gmail.com>
2022-10-01 16:04:55 -0300

    Model de cliente e Endereco

--- src/main/java/com/tecodev/sacola/model/Cliente.java
+++ src/main/java/com/tecodev/sacola/model/Cliente.java
@@ -1,4 +1,23 @@
 package com.tecodev.sacola.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@NoArgsConstructor
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    @Embedded
+    private Endereco endereco;
 }
--- src/main/java/com/tecodev/sacola/model/Endereco.java
+++ src/main/java/com/tecodev/sacola/model/Endereco.java
@@ -1,4 +1,19 @@
 package com.tecodev.sacola.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@NoArgsConstructor
 public class Endereco {
+
+    private String cep;
+    private String complemento;
 }
